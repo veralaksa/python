e468180d4b06c0512e0dbd252485e1d8b694df23
dudung <dudung@gmail.com>
2023-03-11 04:35:26 +0700

    new

---
+++ src/stepin/basics/print/multiple_lines.py
@@ -0,0 +1,21 @@
+print("1st line")
+print("2nd line")
+print("3rd line")
+print()
+print("Another line")
+print()
+print()
+print("Last line")
+
+
+"""
+PS L:\home\python> python .\src\stepin\basics\print\multiple_lines.py
+1st line
+2nd line
+3rd line
+
+Another line
+
+
+Last line
+"""
